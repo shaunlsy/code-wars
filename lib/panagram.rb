2b4ea99e15b6dd89b25257931748d269e3329f73
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-03-03 23:05:32 +0000

    done panagram

--- lib/panagram.rb
+++ lib/panagram.rb
@@ -1,3 +1,12 @@
+# def panagram?(string)
+#   string.downcase.chars.uniq.sort.join.include?(('a'..'z').to_a.join)
+# end
+
+# brilliant solution
+# def panagram?(string)
+#   ('a'..'z').all? { |x| string.downcase.include? (x) } 
+# end
+
 def panagram?(string)
-  return true
+  string.downcase.scan(/[a-z]/).uniq.size == 26
 end
