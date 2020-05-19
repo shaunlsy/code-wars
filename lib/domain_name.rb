692ba16d77e2468216a8158aa22ed4f19caaa3a0
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-05-19 17:35:22 +0800

    between either https or www and dot

--- lib/domain_name.rb
+++ lib/domain_name.rb
@@ -1,4 +1,4 @@
 def domain_name(url)
   # return just the domain name
-  return url[/\.(.*)\./,1]
+  return url[/^(?:https?:\/\/)?(?:www\.)?([^:\.?\n]+)/,1]
 end
--- spec/domain_name_spec.rb
+++ spec/domain_name_spec.rb
@@ -7,6 +7,11 @@ describe "#domain_name" do
 
   it "returns string between two dots" do
     expect(domain_name("https://www.cnet.com")).to eq "cnet"
+    expect(domain_name("http://www.zombie-bites.com")).to eq "zombie-bites"
+  end
+
+  it "returns string between slash and dot" do
+    expect(domain_name("http://github.com/carbonfive/raygun")).to eq "github"
   end
 
 end
