01db575f5207853462a3eb6f018651f9fd8739ec
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-05-19 17:40:54 +0800

    done domain_name kata

--- lib/domain_name.rb
+++ lib/domain_name.rb
@@ -2,3 +2,13 @@ def domain_name(url)
   # return just the domain name
   return url[/^(?:https?:\/\/)?(?:www\.)?([^:\.?\n]+)/,1]
 end
+
+# # better solution
+# def domain_name(url)
+#   regex = /(http|https):\/\/(?:www\.)?(?<domain_name>.*?)\./
+#   url.match(regex)[:domain_name]
+# end
+
+# def domain_name(url)
+#   URI.parse(url).host.split('.').last(2)[0]
+# end
